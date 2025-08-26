650f8e13b86fbe790dbe97541f27686993ef50fa
mulingHT <3466232136@qq.com>
2025-08-26 13:02:42 +0800

    add-shangyi

---
+++ 2024/zhoushangyi/pingtu/src/com/itheima/ui/registerjframe.java
@@ -0,0 +1,15 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class registerjframe extends JFrame {
+    public registerjframe() {
+        this.setSize(488,500);
+        this.setTitle("拼图单机版 注册");
+        this.setAlwaysOnTop(true);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        getContentPane();
+    }
+}
